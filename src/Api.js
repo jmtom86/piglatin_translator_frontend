f59fbb9f87e3453cb2689caa9f1c9af9037b6f89
Jordan <jmtom86@yahoo.com>
2020-02-18 17:50:27 -0800

    Styled frontend and cleaned up code

--- src/Api.js
+++ src/Api.js
@@ -1,10 +1,10 @@
-const API_BASE_ADDRESS = 'https://thawing-tundra-47064.herokuapp.com';
+// const API_BASE_ADDRESS = 'https://thawing-tundra-47064.herokuapp.com';
+const API_BASE_ADDRESS = process.env.API_ADDRESS;
 
 export default class PigLatinApi {
 
 	static translate(words) {
 		const uri = API_BASE_ADDRESS + "/piglatin";
-		console.log(words);
 		return fetch(uri, {
 			method: 'POST',
 			headers: {
--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Api from './Api';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
+import { Container, Row, Col, Form, Button, Table, Navbar } from 'react-bootstrap';
 
 function App() {
   const [translations, setTranslations] = useState([]);
@@ -14,6 +12,7 @@ function App() {
     try {
       const response = await Api.translate({'words': event.target.elements.formWords.value});
       const translation = await response.json();
+      document.getElementById("translator").reset();
       setTranslations([translation, ...translations]);
     } catch (e) {
       console.log(e);
@@ -23,17 +22,51 @@ function App() {
   return (
     <React.Fragment>
       <Container>
-        <Form xs={4} onSubmit={translate}>
-          <Form.Group controlId="formWords">
-            <Form.Label>Type in Sentence to Translate to PigLatin</Form.Label>
-            <Form.Control type="text"/>
-          </Form.Group>
-          <Button variant="primary" type="submit">Translate</Button>
-        </Form>
-        <h3>Translations</h3>
-        {translations.map((t,i) =>
-          <p key={i}>{t.translated}</p>
-        )}
+        <Row id="header" className="justify-content-md-center">
+          <Col md={6}>
+            <h1>Pig Latin Translator</h1>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col md={6}>
+            <Form id="translator" xs={4} onSubmit={translate}>
+              <Form.Group controlId="formWords">
+                <Form.Label>Type in a sentence to translate to Pig Latin</Form.Label>
+                <Form.Control type="text"/>
+              </Form.Group>
+              <Button variant="primary" type="submit">Translate</Button>
+            </Form>
+          </Col>
+        </Row>
+        <Row id="translations_table" className="justify-content-md-center">
+          <Col md={10}>
+            <h3>Translations</h3>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Before</th>
+                  <th>After</th>
+                </tr>
+              </thead>
+              <tbody>
+                {translations.map((t,i) =>
+                  <tr key={i}>
+                    <td>{t.words}</td>
+                    <td>{t.translated}</td>
+                  </tr>
+                )}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+        <Navbar fixed="bottom" bg="light">
+          <Navbar.Brand>
+            <span>Made by: Jordan Tom   
+              <a href="https://github.com/jmtom86/piglatin_translator_frontend" target="_blank">Frontend Github Repo</a>
+              <a href="https://github.com/jmtom86/piglatin_translator" target="_blank">Backend Github Repo</a>
+            </span>
+          </Navbar.Brand>
+        </Navbar>
       </Container>
     </React.Fragment>
   );
